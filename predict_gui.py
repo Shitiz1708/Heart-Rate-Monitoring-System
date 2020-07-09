c710bb8ebbc017364a58443121e4946081deb08b
Shitiz Gupta <guptashitiz17@gmail.com>
2020-07-10 03:46:33 +0530

    initial commit

---
+++ predict_gui.py
@@ -0,0 +1,108 @@
+import cv2
+import os
+from PyQt5 import QtWidgets,QtWidgets,QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QObject,QTimer
+from PyQt5.QtGui import QImage,QPixmap
+import sys
+from WebCam import WebCam
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtWidgets.QApplication.translate(context, text, disambig) 
+
+
+class Ui_GUI(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.default()
+        self.webcam = WebCam()
+    
+    def default(self):
+        self.start.clicked.connect(self.startProcess)
+        timer = QTimer(self)
+        timer.setInterval(int(1000/30))
+        timer.timeout.connect(self.get_frame)
+        timer.start()
+
+    def setupCamera(self):
+        self.cap = cv2.VideoCapture(0)
+    
+    def get_frame(self):
+        ret,frame = self.cap.read()
+        image = QImage(frame, *frame.shape[1::-1], QImage.Format_RGB888).rgbSwapped()
+        pixmap = QPixmap.fromImage(image)
+        self.label.setPixmap(pixmap)
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(980, 621)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetMaximumSize)
+        self.verticalLayout.setSpacing(6)
+        self.verticalLayout.setObjectName("verticalLayout")
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.verticalLayout.addItem(spacerItem)
+        self.lcdNumber = QtWidgets.QLCDNumber(self.centralwidget)
+        self.lcdNumber.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lcdNumber.sizePolicy().hasHeightForWidth())
+        self.lcdNumber.setSizePolicy(sizePolicy)
+        self.lcdNumber.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.lcdNumber.setObjectName("lcdNumber")
+        self.verticalLayout.addWidget(self.lcdNumber)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.verticalLayout.addItem(spacerItem1)
+        self.start = QtWidgets.QPushButton(self.centralwidget)
+        self.start.setObjectName("start")
+        self.verticalLayout.addWidget(self.start)
+        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.verticalLayout.addItem(spacerItem2)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 980, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.start.setText(_translate("MainWindow", "Start"))
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = Ui_GUI()
+    ex.show()
+    sys.exit(app.exec_())
